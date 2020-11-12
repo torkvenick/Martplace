f197a2a76dc41093a8f84fcaa9c7da3bf4916302
torkvenick <oomsnikita@gmail.com>
2020-11-12 17:02:33 +0100

    1.20.11

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 
 /* Создаем переменные для облегчения работы в дальнейшем */
 let gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename');
 
 /* Конвертируем SASS в CSS. Даем задание (task)
 .task() -- это метод (точка, слово, скобки)
 function нужен для того, чтобы метод не работал единожды */
 gulp.task('sass', function(){
-  gulp.src('app/scss/style.scss')
-  .pipe(sass())
-  .pipe(gulp.dest('app/css'))
+
+  /* Откуда будут браться файлы */
+  return gulp.src('app/scss/style.scss')
+  /* Что мы с файлом делаем (в данном случае переименовываем и делаем css минифицированным) */
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(rename({suffix : '.min'}))
+  /* Куда это все выливается */
+        .pipe(gulp.dest('app/css'))
+});
+
+/* Подключаем плагин, который следит за изменениями */
+gulp.task('watch', function(){
+  /* метод: если происходят изменения в style.scss запускается плагин "parallel" */
+  gulp.watch('app/scss/style.scss', gulp.parallel('sass'));
 });
